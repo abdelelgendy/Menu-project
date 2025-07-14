7a7baad5c7d20da9965bfbf4c09d039e58e56726
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-07-14 16:36:46 -0400

    dynamically displaying items

--- app.js
+++ app.js
@@ -76,5 +76,21 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center')
 
 window.addEventListener('DOMContentLoaded', ()=>{
-  console.log('shake nbake')
+  let displayMenu = menu.map((item)=>{
+
+    return ` <article class="menu-item">
+            <img src=${item.img} class="photo" alt=${item.title}>
+            <div class="item-info">
+
+              <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}$</h4>
+              </header>
+              <p>${item.desc}</p>
+            </div>
+           </article>`;
+  });
+    displayMenu = displayMenu.join('')
+      sectionCenter.innerHTML = displayMenu
+
 })
