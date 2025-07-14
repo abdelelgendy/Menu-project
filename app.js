cdba1642bb02872df15da7303827e061aae2bbad
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-07-14 16:41:27 -0400

    refactoring to a function for dynamic menu

--- app.js
+++ app.js
@@ -76,21 +76,27 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center')
 
 window.addEventListener('DOMContentLoaded', ()=>{
-  let displayMenu = menu.map((item)=>{
-
-    return ` <article class="menu-item">
-            <img src=${item.img} class="photo" alt=${item.title}>
-            <div class="item-info">
-
-              <header>
-              <h4>${item.title}</h4>
-              <h4 class="price">${item.price}$</h4>
-              </header>
-              <p>${item.desc}</p>
-            </div>
-           </article>`;
-  });
-    displayMenu = displayMenu.join('')
-      sectionCenter.innerHTML = displayMenu
-
+    displayMenuItems(menu)
 })
+
+
+//function for dynamically rendering menu items
+const displayMenuItems = (MenuItems)=>{
+      let displayMenu = MenuItems.map((item)=>{
+
+      return ` <article class="menu-item">
+              <img src=${item.img} class="photo" alt=${item.title}>
+              <div class="item-info">
+
+                <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}$</h4>
+                </header>
+                <p>${item.desc}</p>
+              </div>
+            </article>`;
+    });
+      displayMenu = displayMenu.join('')
+        sectionCenter.innerHTML = displayMenu
+
+}
